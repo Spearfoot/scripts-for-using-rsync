c4d910be9b0aeef8c45db313a318e2d42256bda9
Keith Nash <Spearfoot@users.noreply.github.com>
2020-08-11 11:54:07 -0500

    Added additional quoting

--- rsync-module.sh
+++ rsync-module.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-if [ $# -ne 3 ]
-then
+if [ $# -ne 3 ]; then
   echo "Error: not enough arguments!"
   echo "Usage is: $0 r_src r_dest r_logfile"
   exit 2
@@ -19,10 +18,9 @@ R_OPTIONS="-rltgoDhv --delete-during --inplace --progress --log-file="${R_LOGFIL
 
 R_EXCLUDE="--exclude vmware.log --exclude vmware-*.log --exclude @eaDir/ --exclude @eaDir --exclude Thumbs.db"
 
-echo "+---------------------------------------------------------------------------------" | tee -a ${R_LOGFILE}
-echo "+ $(date): Copy $R_SRC to $R_DEST" | tee -a ${R_LOGFILE}
-echo "+---------------------------------------------------------------------------------" | tee -a ${R_LOGFILE}
+echo "+---------------------------------------------------------------------------------" | tee -a "${R_LOGFILE}"
+echo "+ $(date): Copy $R_SRC to $R_DEST" | tee -a "${R_LOGFILE}"
+echo "+---------------------------------------------------------------------------------" | tee -a "${R_LOGFILE}"
 rsync ${R_OPTIONS} ${R_EXCLUDE} ${R_SRC} rsync://${R_DEST}
-echo "+ $(date) Transfer completed" | tee -a ${R_LOGFILE}
+echo "+ $(date) Transfer completed" | tee -a "${R_LOGFILE}"
 exit
-
